78f796e185d177bdfc4f4f89343cf5becd55f90d
kanakkadulkar <144000040+kanakkadulkar@users.noreply.github.com>
2024-09-10 16:13:20 +0530

    frontinit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Textform from "./component/textform";
 
 function App() {
-  const [queryText, setQueryText] = useState("");
-  const [response, setResponse] = useState("");
-
-  const handleQuery = async () => {
-    try {
-      const res = await axios.post("http://localhost:5000/query", {
-        query_text: queryText,
-      });
-      setResponse(res.data.response);
-    } catch (error) {
-      console.error("Error querying the API", error);
-    }
-  };
+ 
 
   return (
-    <div className="App">
-      <h1>Query the AI</h1>
-      <input type="text" value={queryText} onChange={e => setQueryText(e.target.value)} />
-      <button onClick={handleQuery}>Submit</button>
-      <div>
-        <h2>Response:</h2>
-        <pre>{response}</pre>
-      </div>
-    </div>
+   <>
+   <Textform/>
+   </>
   );
 }
 
